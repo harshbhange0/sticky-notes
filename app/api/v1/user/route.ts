72aab71ed9fa37436dfcd6cf2cf9be7da00f194c
harshbhane0 <harshbhange123@gmail.com>
2024-04-04 18:45:53 +0530

    typo

--- app/api/v1/user/route.ts
+++ app/api/v1/user/route.ts
@@ -5,23 +5,22 @@ import { db } from "@/lib/db";
 
 export async function GET(req: NextRequest) {
   const session = await getServerSession(authOptions);
-  if (session && session.user.email == process.env.AUTH_USER1) {
-    const user = await db.user.findMany({
-      select: {
-        id: true,
-        name: true,
-        email: true,
-        image: true,
-        notes: {
-          select: {
-            id: true,
-            title: true,
-            content: true,
-          },
+  const user = await db.user.findMany({
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      image: true,
+      notes: {
+        select: {
+          id: true,
+          title: true,
+          content: true,
         },
       },
-    });
-    return NextResponse.json({ auth: true, data: user });
-  }
-  return NextResponse.json({ auth: false });
+    },
+  });
+  return NextResponse.json({ auth: true, data: user });
+
+
 }
